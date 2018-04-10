9d214541c02b46baccc4cc2d67ee87ffe7a18f41
Tilemachos Zoitsas <tilemahoscfu@gmail.com>
2018-04-10 13:07:19 +0300

    Update vending.py

--- vending.py
+++ vending.py
@@ -2,57 +2,55 @@
 # Calculate the change first before giving them.
 
 
-class VendingMachine:
+class Coin:
+	def __init__(self, value, quantity):
+		self.value = value
+		self.quantity = quantity
 
+	def isAvailable(self):
+		return self.quantity > 0
+
+
+class VendingMachine:
 	def __init__(self, coins):
 		self.coins = coins
 
-	def give_change(self, payment, inserted):
-		self.change = round(inserted - payment,2)
-		self.pos = 0
+	def transaction(self):
+		self.payment = float(input("Payment amount: "))
+		self.inserted = float(input("Inserted amount: "))
+		self.change = round(self.inserted - self.payment, 2)
+		self.giveChange()
 
+	def giveChange(self):
+		pos = 0
 		while self.change > 0:
-			if not self.valid_coin_value():
-				break
-			self.give_coin()
+			if self.coins[pos].value > self.change or self.coins[pos].quantity == 0:
+				if pos < len(self.coins) - 1:
+					pos += 1
+				else:
+					print("Sorry, no more change")
+					break
+				continue
 
-	def give_coin(self):
-		print("coin", self.current_coin())
-		self.change -= self.current_coin()
-		self.decrease_quantity()
-
-	def decrease_quantity(self):
-		self.coins[self.pos][1] -= 1
-
-	def valid_coin_value(self):
-		while self.change < self.current_coin() or self.current_coin_quantity() == 0:
-			if self.last_coin():
-				print("Unable to give amount")
-				return False
-			self.pos += 1
-		return True
-
-	def current_coin(self):
-		return self.coins[self.pos][0]
-
-	def current_coin_quantity(self):
-		return self.coins[self.pos][1]
+			print("Give ", self.coins[pos].value)
+			self.change = round(self.change - self.coins[pos].value, 2)
+			self.coins[pos].quantity -= 1
 
 
-	def last_coin(self):
-		return self.pos == len(self.coins)-1
-if __name__ == "__main__":
-	coins = [
-				[2,20], [1,20], [0.5,40],
-				[0.2,40], [0.1,40], [0.05,40]
-			]
 
-	vm = VendingMachine(coins)
-	answer = 'y'
-	while answer == 'y':
-		payment = float(input("Payment: "))
-		inserted = float(input("Value inserted: "))
+coins = []
+coins.append(Coin(2, 20))
+coins.append(Coin(1, 20))
+coins.append(Coin(0.5, 20))
+coins.append(Coin(0.2, 20))
+coins.append(Coin(0.1, 20))
+coins.append(Coin(0.05, 20))
 
-		vm.give_change(payment, inserted)
+vm = VendingMachine(coins)
 
-		answer = input("Continue? ")
+answer = 'y'
+
+while answer == 'y':
+	vm.transaction()
+	answer = input("Continue? ")
+
